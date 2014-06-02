efe65bc99ba4e6c2d2a5e32c86f431cd9b254e77
Danny Hadley <dadley@sizethreestudios.com>
2014-06-02 15:58:33 -0400

    updated local database settings

--- app/config/database.php
+++ app/config/database.php
@@ -7,9 +7,9 @@ return array(
     'mysql' => array(
       'driver'    => 'mysql',
       'host'      => 'localhost',
-      'database'  => 'forge',
-      'username'  => 'forge',
-      'password'  => '',
+      'database'  => 'loftili',
+      'username'  => 'loftili',
+      'password'  => 'password',
       'charset'   => 'utf8',
       'collation' => 'utf8_unicode_ci',
       'prefix'    => '',
