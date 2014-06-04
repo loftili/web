544d573209488d7b033229aec3da44c677054dc0
Danny Hadley <dadley@sizethreestudios.com>
2014-06-04 13:33:05 -0400

    added another hostname to the local dev env lookup

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -3,47 +3,13 @@
 $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
-  'local' => array('homestead', '*.local'),
+  'local' => array('homestead', '*.local', 'dhuws'),
 ));
-
-/*
-|--------------------------------------------------------------------------
-| Bind Paths
-|--------------------------------------------------------------------------
-|
-| Here we are binding the paths configured in paths.php to the app. You
-| should not be changing these here. If you need to change these you
-| may do so within the paths.php file and they will be bound here.
-|
-*/
 
 $app->bindInstallPaths(require __DIR__.'/paths.php');
 
-/*
-|--------------------------------------------------------------------------
-| Load The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load this Illuminate application. We will keep this in a
-| separate location so we can isolate the creation of an application
-| from the actual running of the application with a given request.
-|
-*/
-
-$framework = $app['path.base'].
-                 '/vendor/laravel/framework/src';
+$framework = $app['path.base'].'/vendor/laravel/framework/src';
 
 require $framework.'/Illuminate/Foundation/start.php';
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
 
 return $app;
