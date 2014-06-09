9d2261c9a198ce07b843f153da5b9f0223a1fe25
Danny Hadley <danny@dadleyy.com>
2014-06-09 11:59:49 -0400

    typeography element styles - ul li and code

--- app/views/partial/head.blade.php
+++ app/views/partial/head.blade.php
@@ -3,5 +3,6 @@
 <title>{{ trans('global.title') }} | {{ $title or trans('global.title_message') }}</title>
 <link rel="stylesheet" type="text/css" href="/fonts/index.css" />
 <link rel="stylesheet" type="text/css" href="/css/application.css" />
+<link href='http://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
 <!-- end head partial -->
 
