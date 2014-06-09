6e62c95dba05078efcd559d31e4dd18c5a561516
Danny Hadley <danny@dadleyy.com>
2014-06-09 10:19:52 -0400

    adding post author to blog post

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -6,6 +6,10 @@ class BlogController extends BaseController {
 
   public function home() {
     $posts = get_posts();
+    foreach($posts as $post) {
+      $author_meta = get_user_meta($post->post_author);
+      $post->post_author = $author_meta;
+    }
     return View::make('blog.index')->with('posts', $posts);
   }
 
