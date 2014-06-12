ce790b0ed712f6d720e95e590a4c78524d8b6b37
Danny Hadley <danny@dadleyy.com>
2014-06-12 17:03:46 -0400

    adding wordpress terms migrations

--- app/database/seeds/WordpressTermsSeeder.php
+++ app/database/seeds/WordpressTermsSeeder.php
@@ -4,10 +4,18 @@ class WordpressTermsSeeder extends seeder {
 
   public function run() {
     $this->command->info('- Creating default wp_terms');
+
     $uncat = new WordpressTerm;
     $uncat->name = "Uncategorized";
     $uncat->slug = "uncategorized";
     $uncat->save();
+
+    DB::table('wp_term_taxonomy')->insert(array(
+      'term_id' => 1,
+      'taxonomy' => 'category',
+      'description' => ''
+    ));
+
   }
 
 }
