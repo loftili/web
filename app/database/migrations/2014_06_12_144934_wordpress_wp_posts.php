6569384768a72561d82882d95469adfc57fee69f
Danny Hadley <dadley@sizethreestudios.com>
2014-06-12 22:26:53 -0400

    adding defaults to wp_post columns

--- app/database/migrations/2014_06_12_144934_wordpress_wp_posts.php
+++ app/database/migrations/2014_06_12_144934_wordpress_wp_posts.php
@@ -8,28 +8,28 @@ class WordpressWpPosts extends Migration {
   public function up() {
     Schema::create('wp_posts', function($table) {
       $table->bigIncrements('ID');
-      $table->bigInteger('post_author');
+      $table->bigInteger('post_author')->default(0);
       $table->dateTime('post_date');
       $table->dateTime('post_date_gmt');
-      $table->longText('post_content');
-      $table->text('post_title');
-      $table->text('post_excerpt');
-      $table->string('post_status', 20);
-      $table->string('comment_status', 20);
-      $table->string('ping_status', 20);
-      $table->string('post_password', 20);
-      $table->string('post_name', 200);
-      $table->text('to_ping');
-      $table->text('pinged');
+      $table->longText('post_content')->nullable();
+      $table->text('post_title')->nullable();
+      $table->text('post_excerpt')->nullable();
+      $table->string('post_status', 20)->default('publish');
+      $table->string('comment_status', 20)->default('open');
+      $table->string('ping_status', 20)->default('open');
+      $table->string('post_password', 20)->default('');
+      $table->string('post_name', 200)->default('');
+      $table->text('to_ping')->nullable();
+      $table->text('pinged')->nullable();
       $table->dateTime('post_modified');
       $table->dateTime('post_modified_gmt');
-      $table->longText('post_content_filtered');
-      $table->bigInteger('post_parent');
+      $table->longText('post_content_filtered')->nullable();
+      $table->bigInteger('post_parent')->default(0);
       $table->string('guid', 255);
       $table->integer('menu_order');
-      $table->string('post_type', 20);
-      $table->string('post_mime_type', 100);
-      $table->bigInteger('comment_count');
+      $table->string('post_type', 20)->default('post');
+      $table->string('post_mime_type', 100)->default('');
+      $table->bigInteger('comment_count')->default(0);
     });
   }
 
