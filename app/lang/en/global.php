92640c468784d91a769b19740ddecf48621079de
Danny Hadley <danny@dadleyy.com>
2014-06-13 09:54:28 -0400

    added some meta taggage

--- app/lang/en/global.php
+++ app/lang/en/global.php
@@ -2,7 +2,10 @@
 
 return array(
   'title' => 'loftili',
-  'title_message' => 'the web application for home audio enthusiasts'
+  'title_message' => 'the web application for home audio enthusiasts',
+  'copyright' => date('Y') . ' loftil.i',
+  'keywords' => 'loftili, wifi, audio, home, raspberry pi, open source, laravel, portaudio, libmicrohttp',
+  'description' => 'lofti.li is the audio solution that lets you control what is playing at home from anywhere in the world that you can access the internet.'
 );
 
 ?>
--- app/views/partial/head.blade.php
+++ app/views/partial/head.blade.php
@@ -1,6 +1,11 @@
 <!-- start head partial -->
 <meta charset="utf-8" />
 <title>{{ trans('global.title') }} | {{ $title or trans('global.title_message') }}</title>
+<meta name="keywords" content="{{ trans('global.keywords') }}" />
+<meta name="description" content="{{ $description or trans('global.description') }}" />
+<meta name="copyright" content="{{ trans('global.copyright') }}" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 <link rel="stylesheet" type="text/css" href="/fonts/index.css" />
 <link rel="stylesheet" type="text/css" href="/css/application.css" />
 <link href='http://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
