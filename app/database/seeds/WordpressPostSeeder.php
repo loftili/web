0fb0ecb325370f3f41be7fa6b2fd953fe0e34201
Danny Hadley <dadley@sizethreestudios.com>
2014-06-12 23:29:17 -0400

    fixing some blog marign and padding stuff

--- app/database/seeds/WordpressPostSeeder.php
+++ app/database/seeds/WordpressPostSeeder.php
@@ -5,13 +5,15 @@ class WordpressPostSeeder extends Seeder {
   public function run() {
     $this->command->info('- Creating a few wp_posts');
 
+    $post_content = File::get(app_path().'/database/wp/example_post.txt');
+
     $date_now = new DateTime;
     $post = new BlogPost;
     $post->post_author = 1;
     $post->post_date = $date_now;
     $post->post_date_gmt = $date_now;
-    $post->post_content = 'Hello world';
-    $post->post_title = 'database seed';
+    $post->post_content = $post_content;
+    $post->post_title = 'Nulla augue ipsum, egestas sollicitudin in nisl';
     $post->post_status = 'publish';
     $post->post_name = 'database-seed';
     $post->post_modified = $date_now;
